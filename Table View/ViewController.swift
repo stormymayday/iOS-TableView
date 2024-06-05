348e6d02d73f3604ce244fce85e49e28d57b7da1
stormymayday <stormymayday7@gmail.com>
2024-06-04 21:35:19 -0700

    Table View Demo

--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -7,7 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is cell for \(indexPath.row)"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
